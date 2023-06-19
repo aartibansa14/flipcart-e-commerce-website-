8f7890af797c23455350bba43f73bb7e2acde7d5
Aarti Bansal <111700997+aartibansa14@users.noreply.github.com>
2023-06-19 17:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-let flag=0
-showslide(flag);
-function go(x)
-{
- flag=flag+x;
- showslide(flag);
+
+
+let flag = 0;
+showSlide(flag);
+
+function go(x) {
+  flag = flag + x;
+  showSlide(flag);
 }
 
-function showslide(num){
-    let slides=document.getElementsByClassName('slide');
-    if(num<0)
-    {
-       
-        flag=slides.length-1;
-        num=slides.length-1;
-    }
+function showSlide(num) {
+  let slides = document.querySelectorAll('[id^="slides"]');
+  
+  if (num < 0) {
+    flag = slides.length - 1;
+    num = slides.length - 1;
+  }
 
-   if(num==slides.length)
-   {
-    flag=0;
-    num=0;
-   }
-    for(let i=0;i<slides.length;i++)
-    {
-        slides[i].style.display='none';
-    }
+  if (num == slides.length) {
+    flag = 0;
+    num = 0;
+  }
 
-    slides[num].style.display='block';
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[num].style.display = 'block';
 }
+
+
 
 
